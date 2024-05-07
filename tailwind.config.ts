4abba4dfa96ad9699ac93d75091184782f3585ec
Gustavo Veiga São Leandro <55115090+guta307@users.noreply.github.com>
2024-05-07 10:06:17 -0300

    feat(styles): add color and dimension utilities to global and Tailwind CSS
    - Implement global color schemes in global.css to ensure consistency across all components.
    - Add height, width, and screen size utilities to Tailwind configuration.
    - Define custom colors and responsive dimensions based on project-specific requirements.
    - Ensure all new utilities align with the existing design system and accessibility standards.
    
    This update introduces a comprehensive update to our styling approach, allowing for more flexible and consistent design adjustments across the application.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,86 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
+function toRgba(cssVariable: any) {
+  const color = `var(${cssVariable})`;
+  return ({ opacityValue }: any) => `rgba(${color}, ${opacityValue})`;
+}
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    fontFamily: {
+      mulish: "Poppins, sans-serif",
+    },
+    fontSize: {
+      xs: "var(--font-size-xs)" /*40px*/,
+      sm: "var(--font-size-sm)" /*45px*/,
+      md: "var(--font-size-md)" /*50px*/,
+      xl: "var(--font-size-xl)" /*120px*/,
+    },
+
+    screens: {
+      mobile: "var(--screen-mobile)",
+      desktop: "var(--screen-desktop)",
+    },
+
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        primary: {
+          default: toRgba("--primary") /*237, 5, 144*/,
+        },
+      },
+
+      textColor: {
+        primary: {
+          default: toRgba("--primary") /*237, 5, 144*/,
+        },
+        secondary: {
+          default: toRgba("--secondary") /*66, 9, 225*/,
+        },
+      },
+
+      backgroundColor: {
+        primary: {
+          default: toRgba("--primary") /*237, 5, 144*/,
+        },
+        secondary: {
+          default: toRgba("--secondary") /*66, 9, 225*/,
+        },
+      },
+
+      borderColor: {
+        primary: {
+          default: toRgba("--primary") /*66, 9, 225*/,
+        },
+        secondary: {
+          default: toRgba("--secondary") /*66, 9, 225*/,
+        },
+      },
+
+      // Widths
+      width: {
+        xs: "22",
+        lg: "137px",
+        xl: "252px",
+        "2xl": "492px",
+        "3xl": "819px",
+        "4xl": "842",
+      },
+
+      // Heights
+      height: {
+        xxs: "16px",
+        xs: "18px",
+        sm: "22px",
+        md: "52px",
+        lg: "113px",
+        xl: "252px",
+        "2xl": "397px",
+        "3xl": "775px",
       },
     },
   },
   plugins: [],
 };
-export default config;
