5425cdd5eeb8aa1172ac4ccc4327a2b7797cee80
Gustavo Veiga São Leandro <55115090+guta307@users.noreply.github.com>
2024-05-11 10:45:10 -0300

    feat(styles): integrate TailwindCSS with global styles and shadcn/ui customizations
    Configured tailwind.config.js to include global.css for project-wide style consistency. Enhanced integration with shadcn/ui by extending Tailwind utilities to match our design requirements. Ensured that all customizations align with our branding and accessibility standards.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,22 @@
-function toRgba(cssVariable: any) {
-  const color = `var(${cssVariable})`;
-  return ({ opacityValue }: any) => `rgba(${color}, ${opacityValue})`;
-}
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import type { Config } from "tailwindcss";
+
+const config = {
+  darkMode: ["class"],
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
+  prefix: "",
   theme: {
     fontFamily: {
       mulish: "Poppins, sans-serif",
     },
     fontSize: {
+      "4xs": "var(--font-size-4xs)" /*20px*/,
+      "3xs": "var(--font-size-3xs)" /*25px*/,
+      "2xs": "var(--font-size-2xs)" /*30px*/,
       xs: "var(--font-size-xs)" /*40px*/,
       sm: "var(--font-size-sm)" /*45px*/,
       md: "var(--font-size-md)" /*50px*/,
@@ -24,63 +27,66 @@ module.exports = {
       mobile: "var(--screen-mobile)",
       desktop: "var(--screen-desktop)",
     },
-
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
       colors: {
         primary: {
-          default: toRgba("--primary") /*237, 5, 144*/,
-        },
-      },
-
-      textColor: {
-        primary: {
-          default: toRgba("--primary") /*237, 5, 144*/,
+          default: "#22385C" /*237, 5, 144*/,
         },
         secondary: {
-          default: toRgba("--secondary") /*66, 9, 225*/,
+          default: "#E25813" /*66, 9, 225*/,
+        },
+        light: {
+          default: "#D9D9D9",
         },
       },
-
-      backgroundColor: {
-        primary: {
-          default: toRgba("--primary") /*237, 5, 144*/,
-        },
-        secondary: {
-          default: toRgba("--secondary") /*66, 9, 225*/,
-        },
-      },
-
-      borderColor: {
-        primary: {
-          default: toRgba("--primary") /*66, 9, 225*/,
-        },
-        secondary: {
-          default: toRgba("--secondary") /*66, 9, 225*/,
-        },
-      },
-
       // Widths
       width: {
-        xs: "22",
-        lg: "137px",
-        xl: "252px",
-        "2xl": "492px",
-        "3xl": "819px",
-        "4xl": "842",
+        "3xs": "15px", // Corresponds to 15px
+        "2xs": "18px", // Corresponds to 18px
+        xs: "22px", // Corresponds to 22px
+        sm: "47px", // Corresponds to 47px
+        md: "57px", // Corresponds to 57px
+        lg: "93px", // Corresponds to 93px
+        xl: "137px", // Corresponds to 137px
+        "2xl": "252px", // Corresponds to 252px
+        "3xl": "491px", // Corresponds to 491px
+        "4xl": "819px", // Corresponds to 819px
+        "5xl": "842px", // Corresponds to 842px
       },
-
-      // Heights
       height: {
-        xxs: "16px",
-        xs: "18px",
-        sm: "22px",
-        md: "52px",
-        lg: "113px",
-        xl: "252px",
-        "2xl": "397px",
-        "3xl": "775px",
+        xxs: "16px", // Assuming you meant 16px, as xxs height was not provided
+        xs: "18px", // Corresponds to 18px
+        sm: "22px", // Corresponds to 22px
+        md: "52px", // Corresponds to 52px (adjusted to match closest provided measurement)
+        lg: "113px", // Corresponds to 113px (adjusted for approximation)
+        xl: "252px", // Corresponds to 252px
+        "2xl": "397px", // Corresponds to 397px
+        "3xl": "772px", // Adjusted to 772px to match provided measurements
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
-  plugins: [],
-};
+  plugins: [require("tailwindcss-animate")],
+} satisfies Config;
+
+export default config;
