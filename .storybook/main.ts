56e547346ee0ac2455ebd2e7a70b924fa57250c0
Gustavo Veiga São Leandro <55115090+guta307@users.noreply.github.com>
2024-05-11 10:47:45 -0300

    chore(storybook): configure Storybook to align with project components and styles
    Configured Storybook to load global.css via preview.css ensuring consistent styling across all component previews. This setup allows for an accurate representation of components as they will appear in the production environment, facilitating better component development and testing.

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,12 +1,14 @@
 import type { StorybookConfig } from "@storybook/nextjs";
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: [
+    "../src/stories/**/*.mdx",
+    "../src/stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+  ],
   addons: [
-    "@storybook/addon-onboarding",
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@chromatic-com/storybook",
+    "@storybook/addon-onboarding",
     "@storybook/addon-interactions",
   ],
   framework: {
--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,8 +1,8 @@
 import type { Preview } from "@storybook/react";
-import "../src/style/global.css";
-
+import "../src/style/globals.css";
 const preview: Preview = {
   parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
       matchers: {
         color: /(background|color)$/i,
