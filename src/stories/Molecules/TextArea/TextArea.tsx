000a061f8d3ee1c41072d7d856d2a426f23febac
Gustavo Veiga São Leandro <55115090+guta307@users.noreply.github.com>
2024-05-16 11:43:52 -0300

    feat: Implement TextArea component aligned with application design standards
    - Create a new TextArea component for form inputs, designed to match the application's predefined style guidelines.
    - Includes customizable properties for adjusting the size, border, color, and padding to ensure it integrates seamlessly with existing form elements.
    - Implement responsive design features to ensure that the TextArea component performs well on both desktop and mobile platforms.
    - The component supports text wrapping, maximum character limits, and auto-resizing based on the content length.
    - Accessibility enhancements include keyboard navigability, focus indicators, and aria-labels for screen readers.
    
    This TextArea component will enhance user interaction by providing a consistent and visually appealing text input area across various forms within the app.

---
+++ src/stories/Molecules/TextArea/TextArea.tsx
@@ -0,0 +1,22 @@
+import { Text } from "../../Atoms/Text/Text";
+import { Label } from "@/components/ui/label";
+import { InputProps } from "@/interfaces/Input";
+import { Textarea } from "@/components/ui/textarea";
+
+export const TextAreaComponent = ({ label, id, name }: InputProps) => {
+  return (
+    <div className="grid w-full  items-center gap-5">
+      <Label htmlFor={id}>
+        <Text color="white" type="medium">
+          {"{"} {label} {"}"}
+        </Text>
+      </Label>
+      <Textarea
+        className=" text-3xs resize-none bg-light-default h-[113px]  w-full mobile:h-[48px] "
+        name={name}
+        id={id}
+        placeholder=""
+      />
+    </div>
+  );
+};
