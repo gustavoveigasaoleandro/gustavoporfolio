2fc4d08bf7b24bc11bf1d113e8da6b97b8d3d455
Gustavo Veiga São Leandro <55115090+guta307@users.noreply.github.com>
2024-05-22 13:57:21 -0300

    style: background modification in the active box

--- src/stories/Molecules/Box/Box.tsx
+++ src/stories/Molecules/Box/Box.tsx
@@ -3,7 +3,7 @@ import { BoxProps } from "@/interfaces/Box";
 function SizeGenerator({ type, active }: BoxProps) {
   switch (type) {
     case "normal":
-      return ` w-3xl ${active ? "border-[1px] border-secondary-default shadow-2xl" : ""} h-xl rounded-3xl mobile:w-[224px] mobile:h-lg `;
+      return ` w-3xl ${active ? "bg-red-300 shadow-2xl" : ""}   border-[1px] border-secondary-default h-xl rounded-3xl mobile:w-[224px] mobile:h-lg `;
       break;
     default:
       return "w-4xl h-[397px] rounded-3xl mobile:w-[306px] mobile:h-[234px]";
@@ -15,7 +15,7 @@ export const Box = ({ type, style = "", active = false }: BoxProps) => {
   const size = SizeGenerator({ type, active });
   return (
     <div
-      className={`${size} ${style} embla__slide__number  bg-light-default`}
+      className={`${size} ${style} embla__slide__number bg-light-default`}
     ></div>
   );
 };
