f598b7e65085ad2a6bd154d7d7b943a41d6fb4d6
Gustavo Veiga São Leandro <55115090+guta307@users.noreply.github.com>
2024-05-28 15:09:41 -0300

    refactor: Update PopUpTrigger component with fixed positioning and responsive styles
    - Added fixed positioning to the PopUpTrigger component with bottom-right alignment.
    - Updated component to use responsive widths and heights for desktop and mobile views.
    - Ensured consistent alignment and appearance across different screen sizes.
    - Applied z-index to ensure the component remains on top of other elements.

--- src/stories/Organisms/PopUpTrigger/PopUpTrigger.tsx
+++ src/stories/Organisms/PopUpTrigger/PopUpTrigger.tsx
@@ -3,9 +3,9 @@ import Image from "next/image";
 
 export const PopUpTrigger = () => {
   return (
-    <div className=" w-xl flex justify-center align-middle items-center h-[137px] mobile:w-[56px] mobile:h-[56px] bg-secondary-default rounded-full">
+    <div className=" z-10 desktop:w-xl fixed bottom-6 right-1 flex justify-center align-middle items-center desktop:h-[137px] mobile:w-[56px] mobile:h-[56px] bg-secondary-default rounded-full">
       <Image
-        className="w-[96px] mobile:w-[40px]"
+        className="desktop:w-[96px] mobile:w-[40px]"
         src={PopUpIcon}
         alt="PopUp Icon"
       />
