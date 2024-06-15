a9986310e17fbc6a8e0c24754cc7b21a47de680b
Gustavo Veiga São Leandro <55115090+guta307@users.noreply.github.com>
2024-06-15 11:40:01 -0300

    Enhance PopUpTrigger component with modal functionality
    - Added a `useState` hook to manage the state of the modal (ModalActive).
    - Implemented a `TriggerModal` function to toggle the modal's visibility.
    - Imported the PopUp component to render it when the modal is active.
    - Added conditional rendering for the PopUp component within a fixed div that centers the modal on the screen.
    - Updated the main trigger div to include an `onClick` event handler that calls `TriggerModal` to toggle the modal state.
    - Ensured that the trigger icon (PopUpIcon) remains responsive with appropriate classes for desktop and mobile sizes.
    - Included a new CSS class `float` for potential floating animation effects on the trigger button.
    - Integrated styles from `style.css` to maintain consistency and visual appeal.

--- src/stories/Organisms/PopUpTrigger/PopUpTrigger.tsx
+++ src/stories/Organisms/PopUpTrigger/PopUpTrigger.tsx
@@ -1,14 +1,34 @@
+"use client";
 import PopUpIcon from "@/assets/images/PopupTrigger/PopupTrigger.svg";
 import Image from "next/image";
-
+import "./style.css";
+import { useState } from "react";
+import { PopUp } from "../PopUp/PopUp";
 export const PopUpTrigger = () => {
+  const [ModalActive, setModalActive] = useState(false);
+
+  const TriggerModal = () => {
+    setModalActive(!ModalActive);
+  };
+
   return (
-    <div className=" z-10 desktop:w-xl fixed bottom-6 right-1 flex justify-center align-middle items-center desktop:h-[137px] mobile:w-[56px] mobile:h-[56px] bg-secondary-default rounded-full">
-      <Image
-        className="desktop:w-[96px] mobile:w-[40px]"
-        src={PopUpIcon}
-        alt="PopUp Icon"
-      />
-    </div>
+    <>
+      {ModalActive && (
+        <div className="z-20 fixed top-0 left-0 right-0 bottom-0 m-auto pt-10 flex justify-center items-center">
+          <PopUp />
+        </div>
+      )}
+
+      <div
+        onClick={TriggerModal}
+        className="z-30 desktop:w-xl fixed bottom-6 right-1 flex justify-center align-middle items-center desktop:h-[137px] mobile:w-[56px] mobile:h-[56px] bg-secondary-default rounded-full float"
+      >
+        <Image
+          className="desktop:w-[96px] mobile:w-[40px]"
+          src={PopUpIcon}
+          alt="PopUp Icon"
+        />
+      </div>
+    </>
   );
 };
