2fa68f021d6abf667c2f6be7a2c937261fe0af70
Gustavo Veiga São Leandro <55115090+guta307@users.noreply.github.com>
2024-06-15 12:04:42 -0300

    storybook: add story configuration for Logos component within a Box decorator

---
+++ src/stories/Atoms/Logos/Logos.stories.tsx
@@ -0,0 +1,15 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { Box } from "@/stories/Molecules/Box/Box";
+import { Logos } from "./Logos";
+const meta: Meta<any> = {
+  title: "Atoms/Logos",
+  decorators: [(Story) => <Box type="normal">{<Story />}</Box>],
+  component: Logos,
+  argTypes: {},
+};
+
+export default meta;
+
+export const Primary: StoryObj<any> = {
+  args: {},
+};
