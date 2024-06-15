dfa91f68096e58cd878139d8add84249628d5f46
Gustavo Veiga São Leandro <55115090+guta307@users.noreply.github.com>
2024-06-15 12:28:10 -0300

    feat: enhance homepage with new components and animations

--- src/app/pages/home/page.tsx
+++ src/app/pages/home/page.tsx
@@ -6,32 +6,53 @@ import { SkillMeter } from "@/stories/Organisms/SkillMeter/SkillMeter";
 import { EmblaOptionsType } from "embla-carousel";
 import { Carroussel } from "@/stories/Organisms/Carroussel/Carroussel";
 import { PopUpTrigger } from "@/stories/Organisms/PopUpTrigger/PopUpTrigger";
+import ParticlesBackground from "@/stories/Molecules/Background/Background";
+import { VerticalCarousel } from "@/stories/Organisms/VerticalCarroussel/VerticalCarroussel";
+import { Logos } from "@/stories/Atoms/Logos/Logos";
+import { WorkGif } from "@/stories/Atoms/GIFS/Work/WorkGif";
+import { StudyGif } from "@/stories/Atoms/GIFS/Study/StudyGif";
 
 const OPTIONS: EmblaOptionsType = {
   axis: "y",
 };
-const slidesData = [{ type: "normal" }, { type: "normal" }, { type: "normal" }];
+const slidesData = [
+  {
+    key: 1,
+    type: "normal",
+    children: <StudyGif />,
+  },
+  { key: 2, type: "normal", children: <Logos /> },
+  {
+    key: 3,
+    type: "normal",
+    children: <WorkGif />,
+  },
+];
 
-const home = () => {
+const Home = () => {
   return (
     <div className=" overflow-x-hidden">
       <Header />
       <PopUpTrigger />
-      <div className="w-screen overflow-hidden desktop:min-h-screen tablet:h-fit laptop:h-fit mobile:h-fit bg-primary-default">
-        <div className=" w-full desktop:min-h-screen tablet:h-fit laptop:h-fit  l mobile:h-fit bg-main desktop:pt-[124px] mobile:pt-[57px] flex justify-center">
-          <Image className=" w-[80%]" src={LogoImg} alt="image from the logo" />
+      <ParticlesBackground />
+      <div className="w-screen overflow-hidden h-screen  align-middle items-center flex justify-center bg-primary-default">
+        <div className=" w-full h-fit flex justify-center align-middle  ">
+          <Image
+            className=" z-10 desktop:w-[50%] mobile:w-[100%]"
+            src={LogoImg}
+            alt="image from the logo"
+          />
         </div>
       </div>
-      <div className="w-screen desktop:min-h-screen tablet:h-fit laptop:h-fit mobile:h-fit bg-primary-default">
-        <div className=" flex desktop:flex-row mobile:flex-col justify-evenly w-screen desktop:min-h-screen tablet:h-fit laptop:h-fit  l mobile:h-fit ">
-          <section className=" flex flex-col justify-start items-center  ">
+      <div className="w-screen tablet:h-screen mobile:h-fit bg-primary-default">
+        <div className=" flex mobile:flex-col tablet:flex-col laptop:flex-col desktop:flex-row  justify-evenly w-screen  mobile:h-fit ">
+          <section className=" flex flex-col justify-start items-center   mobile:pb-10 tablet:pb-0">
             <HeaderComponent
               className=" text-center font-semibold "
               color="secondary"
             >
               HABILIDADES
             </HeaderComponent>
-
             <div className=" flex flex-col w-full justify-center items-center ">
               <SkillMeter progress={90} skill={"html"} />
               <SkillMeter progress={30} skill={"html"} />
@@ -41,16 +62,19 @@ const home = () => {
             </div>
           </section>
 
-          <section className=" text-center flex-col justify-center items-center align-middle h-full ">
+          <section className=" text-center z-10 flex-col justify-center items-center align-middle h-full mobile:pb-10 table:pb-0 ">
             <HeaderComponent className=" font-semibold m-0 " color="secondary">
               EXPERIÊNCIAS
             </HeaderComponent>
+            <div className="mobile:w-[100vw] tablet:w-[50vw] flex flex-col justify-center h-[50vh] mx-auto my-0">
+              <VerticalCarousel slidesData={slidesData} />
+            </div>
           </section>
         </div>
       </div>
-      <div className="w-screen h-screen bg-primary-default">
+      <div className="w-screen mobile:h-fit mobile:pb-10 tablet:h-screen bg-primary-default">
         <div className=" flex flex-col justify-evenly items-center w-full h-full">
-          <HeaderComponent className=" font-semibold m-0" color="secondary">
+          <HeaderComponent className="font-semibold m-0" color="secondary">
             PROJETOS
           </HeaderComponent>
           <Carroussel />
@@ -60,4 +84,4 @@ const home = () => {
   );
 };
 
-export default home;
+export default Home;
