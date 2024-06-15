6cca355ea2802877195410d6c17baa0251df4f7c
Gustavo Veiga São Leandro <55115090+guta307@users.noreply.github.com>
2024-06-15 11:40:59 -0300

    Refactor PopUp component for improved responsiveness and styling
    - Updated import paths for `Card`, `CardContent`, `CardFooter`, and `CardHeader` from `@/components/ui/card`.
    - Kept `Label` import from `@/components/ui/label`.
    - Included `AvatarIcon`, `HeaderComponent`, `InputComponent`, and `TextAreaComponent` from respective paths.
    - Modified `Card` component className to be responsive, adjusting `rounded` and `size` properties based on device type:
      - `mobile:rounded-[30px]` for mobile devices
      - `laptop:rounded-[70px]` for laptops
      - `tablet:w-5xl tablet:h-3xl` for tablets
      - `mobile:w-[342px] mobile:h-[314px]` for mobile devices
    - Maintained shadow and background color properties: `shadow-2xl` and `bg-secondary-default`.
    - Adjusted form gap between elements based on device type:
      - `mobile:gap-2` for mobile devices
      - `laptop:gap-12` for laptops
    - Retained structure and functionality of the form with `InputComponent` for `name` and `email` fields, and `TextAreaComponent` for the `subject` field.
    - Ensured `CardHeader` contains the `AvatarIcon` and `HeaderComponent` with appropriate styling.
    - Added responsive classes to ensure the form layout adapts to different screen sizes while maintaining visual appeal and functionality.

--- src/stories/Organisms/PopUp/PopUp.tsx
+++ src/stories/Organisms/PopUp/PopUp.tsx
@@ -1,10 +1,8 @@
 import {
   Card,
   CardContent,
-  CardDescription,
   CardFooter,
   CardHeader,
-  CardTitle,
 } from "@/components/ui/card";
 import { Label } from "@/components/ui/label";
 import { AvatarIcon } from "@/stories/Molecules/Avatar/Avatar";
@@ -14,7 +12,7 @@ import { TextAreaComponent } from "@/stories/Molecules/TextArea/TextArea";
 
 export const PopUp = () => {
   return (
-    <Card className=" shadow-2xl rounded-[70px] w-5xl h-3xl bg-secondary-default">
+    <Card className=" shadow-2xl mobile:rounded-[30px] laptop:rounded-[70px]  tablet:w-5xl tablet:h-3xl  mobile:w-[342px] mobile:h-[314px] bg-secondary-default">
       <CardHeader className="flex justify-center items-center w-full  pb-1 m-0">
         <AvatarIcon url="https://avatars.githubusercontent.com/u/55115090?v=4" />
       </CardHeader>
@@ -24,7 +22,7 @@ export const PopUp = () => {
         </HeaderComponent>
       </CardHeader>
       <CardContent>
-        <form className="flex flex-col gap-5">
+        <form className="flex flex-col mobile:gap-2 laptop:gap-12">
           <div className="flex w-full justify-evenly items-center gap-4">
             <InputComponent
               id="name"
