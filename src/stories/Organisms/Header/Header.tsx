e7aab98a6cc4920ed8cde56c8c5ff9a29f43928f
Gustavo Veiga São Leandro <55115090+guta307@users.noreply.github.com>
2024-06-15 11:44:29 -0300

    Add scroll-dependent header visibility and animation
    - Added `use client` directive at the beginning of the file to ensure proper client-side rendering.
    - Imported `useEffect` and `useState` hooks from React to manage the state and handle side effects.
    - Imported `Text` component and `DrawerComponent` from the specified paths.
    - Imported custom styles from `"./style.css"` for additional styling and animation effects.
    - Introduced a new state variable `isAtTop` to track whether the user has scrolled away from the top of the page.
    - Created a `handleScroll` function that updates the `isAtTop` state based on the scroll position.
    - Added an event listener for the `scroll` event within a `useEffect` hook to invoke the `handleScroll` function.
    - Ensured the event listener is cleaned up when the component unmounts by returning a cleanup function from `useEffect`.
    - Updated the header's class name dynamically based on the `isAtTop` state to toggle visibility and apply a slide-down animation.
    - The header remains visible at the top of the page and hides when the user scrolls down, enhancing user experience and interface aesthetics.
    
    This commit improves the `Header` component by adding scroll-dependent visibility and animation. The header smoothly slides down when the user is at the top of the page and hides when scrolling down, providing a cleaner and more dynamic interface.

--- src/stories/Organisms/Header/Header.tsx
+++ src/stories/Organisms/Header/Header.tsx
@@ -1,10 +1,32 @@
+"use client";
+import React, { useEffect, useState } from "react";
 import { Text } from "@/stories/Atoms/Text/Text";
 import { DrawerComponent } from "@/stories/Molecules/Drawer/Drawer";
-import { Drawer } from "vaul";
-
+import "./style.css";
 export const Header = () => {
+  const [isAtTop, setIsAtTop] = useState(true);
+
+  const handleScroll = () => {
+    if (window.scrollY === 0) {
+      setIsAtTop(true);
+    } else {
+      setIsAtTop(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <header className=" w-screen z-10 fixed top-0 flex align-middle items-center pr-[2%] pl-[2%] justify-between bg-primary-darker desktop:h-[124px] mobile:h-[57px]">
+    <header
+      className={`w-screen z-20 flex align-middle items-center pr-[2%] pl-[2%] justify-between bg-primary-darker desktop:h-[124px] mobile:h-[57px] ${
+        isAtTop ? "header-slide-down" : "hidden"
+      }`}
+    >
       <div className="flex flex-wrap max-w-[50%]">
         <Text color="white">
           <span className=" text-secondary-default">Gustavo</span> Veiga São
