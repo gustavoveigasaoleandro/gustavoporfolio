7949f2eb800ca6cc69502cdbcd92b85cb94192b0
Gustavo Veiga São Leandro <55115090+guta307@users.noreply.github.com>
2024-12-13 17:13:19 -0300

    feat: enhance DrawerComponent with loading state management and navigation links
    - Integrated `useLoading` context to manage the loading state during navigation.
    - Added `handleLinkClick` function to trigger the loading screen when navigating to a different route.
    - Updated navigation links to use `onClick` for handling custom behaviors.
    - Included a `data-testid` attribute for testing the drawer component and close button.
    - Retained the existing responsive styles and layout for the drawer.

--- src/stories/Molecules/Drawer/Drawer.tsx
+++ src/stories/Molecules/Drawer/Drawer.tsx
@@ -1,5 +1,6 @@
-import * as React from "react";
+"use client";
 
+import * as React from "react";
 import MenuIcon from "@/assets/images/sidebar/menu.svg";
 import {
   Drawer,
@@ -8,32 +9,72 @@ import {
   DrawerTrigger,
 } from "@/components/ui/drawer";
 import { Text } from "@/stories/Atoms/Text/Text";
-import Link from "next/link";
 import Image from "next/image";
+import Link from "next/link";
+import { useLoading } from "@/context/LoadingContext";
 
 export function DrawerComponent() {
+  const { setIsLoading } = useLoading();
+
+  const handleLinkClick = (href: string) => {
+    // Verifica se o link é diferente da rota atual
+    console.log(window.location.pathname);
+    console.log(href);
+    if (window.location.pathname !== href) {
+      setIsLoading(true); // Ativa a tela de carregamento
+    }
+
+    // Fecha o Drawer após o clique
+    const drawerCloseButton = document.querySelector(
+      '[data-testid="drawer-close"]'
+    ) as HTMLElement;
+    if (drawerCloseButton) drawerCloseButton.click();
+  };
+
   return (
-    <Drawer>
-      <DrawerTrigger asChild>
-        <button>
-          <Image
-            className="desktop:w-[30px] cursor-pointer mobile:[21px]"
-            src={MenuIcon}
-            alt="Menu button"
-          />
-        </button>
-      </DrawerTrigger>
-      <DrawerContent className="flex flex-row justify-end items-end">
-        <div className=" flex-grow h-full">
-          <DrawerClose className="w-full h-full"></DrawerClose>
-        </div>
-        <nav className=" bg-primary-default desktop:w-[35%] mobile:w-[100%] h-full flex flex-col justify-center align-middle items-center selection:text-center cursor-pointer">
-          <Text color="white">início</Text>
-          <Text color="white">projetos</Text>
-          <Text color="white">experiência</Text>
-          <Text color="white">habilidades</Text>
-        </nav>
-      </DrawerContent>
-    </Drawer>
+    <div data-testid="drawer-component">
+      <Drawer>
+        <DrawerTrigger asChild>
+          <button>
+            <Image
+              className="desktop:w-[30px] cursor-pointer mobile:[21px]"
+              src={MenuIcon}
+              alt="Menu button"
+            />
+          </button>
+        </DrawerTrigger>
+        <DrawerContent className="flex flex-row justify-end items-end">
+          <div className="flex-grow h-full">
+            <DrawerClose data-testid="drawer-close" className="w-full h-full" />
+          </div>
+          <nav className="bg-primary-default desktop:w-[35%] mobile:w-[100%] h-full flex flex-col justify-center align-middle items-center selection:text-center cursor-pointer">
+            <Text color="white">
+              <Link
+                href="/pages/home"
+                onClick={() => handleLinkClick("/pages/home")}
+              >
+                Início
+              </Link>
+            </Text>
+            <Text color="white">
+              <Link
+                href="/pages/studies"
+                onClick={() => handleLinkClick("/pages/studies")}
+              >
+                Cursos
+              </Link>
+            </Text>
+            <Text color="white">
+              <Link
+                href="/pages/experiences"
+                onClick={() => handleLinkClick("/pages/experience")}
+              >
+                Experiências
+              </Link>
+            </Text>
+          </nav>
+        </DrawerContent>
+      </Drawer>
+    </div>
   );
 }
