ae2fb83177a8c477cd08deef62e38a060a19bb0a
Gustavo Veiga São Leandro <55115090+guta307@users.noreply.github.com>
2024-12-13 17:11:51 -0300

    style: update AvatarIcon component for improved tablet responsiveness
    - Adjusted the `Avatar` class to include explicit width and height styles for tablet devices (`tablet:w-[180px] tablet:h-[180px]`).
    - Replaced `desktop` breakpoint with `tablet` for a more focused responsive design.
    - Maintained existing styles for mobile devices and overall shadow effect.
    - Ensured consistency in fallback and image handling for the avatar.

--- src/stories/Molecules/Avatar/Avatar.tsx
+++ src/stories/Molecules/Avatar/Avatar.tsx
@@ -3,7 +3,7 @@ import { AvatarProps } from "@/interfaces/Avatar";
 
 export const AvatarIcon = ({ url }: AvatarProps) => {
   return (
-    <Avatar className="desktop:w-2xl  desktop:h-xl mobile:w-lg mobile:h-[93px] shadow-2xl m-0 p-0">
+    <Avatar className="tablet:w-[180px]  tablet:h-[180px] mobile:w-lg mobile:h-[93px] shadow-2xl m-0 p-0">
       <AvatarImage src={url} />
       <AvatarFallback>GV</AvatarFallback>
     </Avatar>
