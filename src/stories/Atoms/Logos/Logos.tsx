f1882c1e48dc5c9303c612a1b56200fcc21416e8
Gustavo Veiga São Leandro <55115090+guta307@users.noreply.github.com>
2024-12-13 17:09:26 -0300

    style: enhance Logos component with hover effects and animation delays
    - Added `hover:scale-125` to icons for a smooth zoom effect on hover.
    - Included `transition-transform` for smooth scaling transitions.
    - Applied `animationDelay` to stagger the animations for each icon, creating a dynamic appearance.
    - Ensured consistent alignment and spacing using `flex` and `justify-evenly`.
    - Maintained responsive layout with percentage-based widths for icons.

--- src/stories/Atoms/Logos/Logos.tsx
+++ src/stories/Atoms/Logos/Logos.tsx
@@ -4,82 +4,89 @@ import DockerIcon from "@/assets/images/Logo/docker.svg";
 import NextIcon from "@/assets/images/Logo/next.svg";
 import ReactIcon from "@/assets/images/Logo/react.svg";
 import StorybookIcon from "@/assets/images/Logo/storybook.svg";
-import TaiwlindCss from "@/assets/images/Logo/tailwindcss.svg";
+import TailwindCssIcon from "@/assets/images/Logo/tailwindcss.svg";
 import Image from "next/image";
 
 export const Logos = () => {
   return (
     <>
-      <div className=" w-full relative flex flex-row items-center  justify-evenly">
-        <div className=" w-[15%]  transition-transform transform hover:scale-125 ">
+      <div className="w-full relative flex flex-row items-center justify-evenly">
+        <div className="w-[15%] transition-transform transform hover:scale-125">
           <Image
             src={TypescriptIcon}
             alt="icon for the typescript expertise"
-            className=" w-full"
+            className="w-full float"
             style={{
               objectFit: "contain",
+              animationDelay: "0s", // Sem delay
             }}
           />
         </div>
-        <div className=" w-[15%]  transition-transform transform hover:scale-125 ">
+        <div className="w-[15%] transition-transform transform hover:scale-125">
           <Image
             src={JavascriptIcon}
             alt="icon for the javascript expertise"
-            className=" w-full"
+            className="w-full float"
             style={{
               objectFit: "contain",
+              animationDelay: "0.3s", // 0.3s de delay
             }}
           />
         </div>
-        <div className=" w-[15%]  transition-transform transform hover:scale-125 ">
+        <div className="w-[15%] transition-transform transform hover:scale-125">
           <Image
             src={DockerIcon}
             alt="icon for the docker expertise"
-            className=" w-full"
+            className="w-full float"
             style={{
               objectFit: "contain",
+              animationDelay: "0.6s", // 0.6s de delay
             }}
           />
         </div>
-        <div className="transition-transform transform hover:scale-125 ">
+        <div className="w-[15%] transition-transform transform hover:scale-125">
           <Image
             src={StorybookIcon}
             alt="icon for the storybook expertise"
-            className=" w-full"
+            className="w-full float"
             style={{
               objectFit: "contain",
+              animationDelay: "0.9s", // 0.9s de delay
             }}
           />
         </div>
       </div>
-      <div className=" w-full relative flex flex-row items-center  justify-evenly">
-        <div className=" w-[15%]  transition-transform transform hover:scale-125 ">
+      <div className="w-full relative flex flex-row items-center justify-evenly">
+        <div className="w-[15%] transition-transform transform hover:scale-125">
           <Image
             src={NextIcon}
             alt="icon for the next.js expertise"
-            className=" w-full"
+            className="w-full float"
             style={{
               objectFit: "contain",
+              animationDelay: "0s", // 1.2s de delay
             }}
           />
         </div>
-        <div className=" w-[15%]  transition-transform transform hover:scale-125 ">
+        <div className="w-[15%] transition-transform transform hover:scale-125">
           <Image
             src={ReactIcon}
             alt="icon for the react expertise"
-            className=" w-full"
+            className="w-full float"
             style={{
               objectFit: "contain",
+              animationDelay: "0.6s", // 1.5s de delay
             }}
           />
         </div>
-        <div className=" w-[15%]  transition-transform transform hover:scale-125 ">
+        <div className="w-[15%] transition-transform transform hover:scale-125">
           <Image
-            src={TaiwlindCss}
+            src={TailwindCssIcon}
             alt="icon for the tailwindcss expertise"
-            className=" w-full"
+            className="w-full float"
             style={{
               objectFit: "contain",
+              animationDelay: "0.9s", // 1.8s de delay
             }}
           />
         </div>
