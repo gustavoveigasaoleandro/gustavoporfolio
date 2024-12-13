0689c1496596662b3f6c7c67aef31d91262664b7
Gustavo Veiga São Leandro <55115090+guta307@users.noreply.github.com>
2024-12-13 17:02:06 -0300

    feat: enhance Home page with animations and detailed introduction
    - Added `TextAnimation` and `Text` components for an engaging introductory section.
    - Updated the hero section to include a developer biography and animated text.
    - Improved the skills section with categorized progress bars (e.g., Frontend, Backend, DevOps).
    - Maintained core sections for Experiences and Projects with improved layout adjustments.
    - Enhanced user engagement with additional animations and descriptive elements.

--- src/app/pages/home/page.tsx
+++ src/app/pages/home/page.tsx
@@ -8,9 +8,11 @@ import { Carroussel } from "@/stories/Organisms/Carroussel/Carroussel";
 import { PopUpTrigger } from "@/stories/Organisms/PopUpTrigger/PopUpTrigger";
 import ParticlesBackground from "@/stories/Molecules/Background/Background";
 import { VerticalCarousel } from "@/stories/Organisms/VerticalCarroussel/VerticalCarroussel";
-import { Logos } from "@/stories/Atoms/Logos/Logos";
+import { Logos } from "@/stories/Organisms/Logos/Logos";
 import { WorkGif } from "@/stories/Atoms/GIFS/Work/WorkGif";
 import { StudyGif } from "@/stories/Atoms/GIFS/Study/StudyGif";
+import { TextAnimation } from "@/stories/Atoms/TextAnimation/TextAnimation";
+import { Text } from "@/stories/Atoms/Text/Text";
 
 const OPTIONS: EmblaOptionsType = {
   axis: "y",
@@ -35,16 +37,54 @@ const Home = () => {
       <Header />
       <PopUpTrigger />
       <ParticlesBackground />
-      <div className="w-screen overflow-hidden h-screen  align-middle items-center flex justify-center bg-primary-default">
-        <div className=" w-full h-fit flex justify-center align-middle  ">
+      <div className="w-screen  overflow-hidden min-h-screen h-fit mobile:pb-[5%] tablet:pb-0 align-middle items-center flex justify-center bg-primary-default">
+        <div className=" w-full h-fit flex mobile:justify-center mobile:items-center laptop:justify-start mobile:flex-col tablet:flex-row items-center align-middle  ">
           <Image
-            className=" z-10 desktop:w-[50%] mobile:w-[100%]"
+            className=" z-10 tablet:w-[50%]  mobile:w-[100%]"
             src={LogoImg}
             alt="image from the logo"
           />
+          <div className="flex flex-col w-[70%]">
+            <HeaderComponent type="small" color="secondary">
+              <TextAnimation
+                text={`> Iniciando apresentação...`}
+                repeatCount={0} // Ou o número de repetições desejado
+              />
+            </HeaderComponent>
+            <div className="flex  tablet:flex-row  ">
+              <HeaderComponent type="small" color="white">
+                <TextAnimation
+                  text={` Gustavo Veiga São Leandro`}
+                  repeatCount={0} // Ou o número de repetições desejado
+                  delay={2000}
+                />
+              </HeaderComponent>
+            </div>
+
+            <div className="flex flex-col">
+              <HeaderComponent type="small" color="secondary">
+                <TextAnimation
+                  text={`> Sobre mim:`}
+                  repeatCount={0} // Ou o número de repetições desejado
+                  delay={4000}
+                />
+              </HeaderComponent>
+              <Text color="white" type="small">
+                <TextAnimation
+                  text={`Sou desenvolvedor formado em Análise e Desenvolvimento de
+                Sistemas, com experiência em React, Next.js, Node.js e Spring
+                Boot. Trabalho com APIs, microserviços e infraestrutura
+                escalável usando Docker e AWS, focando em soluções completas e
+                bem documentadas.`}
+                  repeatCount={0} // Ou o número de repetições desejado
+                  delay={6000}
+                />
+              </Text>
+            </div>
+          </div>
         </div>
       </div>
-      <div className="w-screen tablet:h-screen mobile:h-fit bg-primary-default">
+      <div className="w-screen min-h-[600px] tablet:h-screen mobile:h-fit bg-primary-default">
         <div className=" flex mobile:flex-col tablet:flex-col laptop:flex-col desktop:flex-row  justify-evenly w-screen  mobile:h-fit ">
           <section className=" flex flex-col justify-start items-center   mobile:pb-10 tablet:pb-0">
             <HeaderComponent
@@ -54,11 +94,9 @@ const Home = () => {
               HABILIDADES
             </HeaderComponent>
             <div className=" flex flex-col w-full justify-center items-center ">
-              <SkillMeter progress={90} skill={"html"} />
-              <SkillMeter progress={30} skill={"html"} />
-              <SkillMeter progress={50} skill={"html"} />
-              <SkillMeter progress={74} skill={"html"} />
-              <SkillMeter progress={23} skill={"html"} />
+              <SkillMeter progress={80} skill={"Frontend"} />
+              <SkillMeter progress={30} skill={"DevOps"} />
+              <SkillMeter progress={70} skill={"Backend"} />
             </div>
           </section>
 
@@ -66,13 +104,13 @@ const Home = () => {
             <HeaderComponent className=" font-semibold m-0 " color="secondary">
               EXPERIÊNCIAS
             </HeaderComponent>
-            <div className="mobile:w-[100vw] tablet:w-[50vw] flex flex-col justify-center h-[50vh] mx-auto my-0">
+            <div className="mobile:w-[100vw] min-h-[300px] tablet:w-[100vw] laptop:w-[50vw] flex flex-col justify-center h-[50vh] mx-auto my-0">
               <VerticalCarousel slidesData={slidesData} />
             </div>
           </section>
         </div>
       </div>
-      <div className="w-screen mobile:h-fit mobile:pb-10 tablet:h-screen bg-primary-default">
+      <div className="w-screen min-h-[600px] mobile:h-fit mobile:pb-10 tablet:h-screen bg-primary-default">
         <div className=" flex flex-col justify-evenly items-center w-full h-full">
           <HeaderComponent className="font-semibold m-0" color="secondary">
             PROJETOS
