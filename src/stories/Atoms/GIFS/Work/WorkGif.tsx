f341aec1634ba35c8fd8eea298f66a6019e9bc9d
Gustavo Veiga São Leandro <55115090+guta307@users.noreply.github.com>
2024-12-13 17:07:29 -0300

    refactor: remove Link component from WorkGif for simplified structure
    - Removed the `Link` component wrapping the `TypeAnimation` in `WorkGif`.
    - Simplified the component to focus solely on the animation sequence.
    - Retained the `TypeAnimation` configuration for dynamic text transitions.
    - Improved clarity and reduced complexity by removing unnecessary navigation logic.

--- src/stories/Atoms/GIFS/Work/WorkGif.tsx
+++ src/stories/Atoms/GIFS/Work/WorkGif.tsx
@@ -1,4 +1,5 @@
 "use client";
+import Link from "next/link";
 import React from "react";
 import { TypeAnimation } from "react-type-animation";
 
@@ -6,25 +7,27 @@ export const WorkGif = () => {
   return (
     <div className="w-full h-full flex items-center justify-center bg-light-default bg-cover bg-center transition-all duration-1000">
       <div className="absolute w-full h-full flex justify-center items-center">
-        <TypeAnimation
-          sequence={[
-            "TRABALHOS",
-            1000,
-            "PROJETOS",
-            2000,
-            "DESAFIOS",
-            1000,
-            "TAREFAS",
-            2000,
-            "OBJETIVOS",
-            1000,
-            "MISSÕES",
-            2000,
-          ]}
-          speed={10}
-          className="desktop:text-xs mobile:text-3xs text-center tablet:text-3xs laptop:text-3xs font-bold text-secondary-default"
-          repeat={Infinity}
-        />
+        <Link href={"/experiences"}>
+          <TypeAnimation
+            sequence={[
+              "TRABALHOS",
+              1000,
+              "PROJETOS",
+              2000,
+              "DESAFIOS",
+              1000,
+              "TAREFAS",
+              2000,
+              "OBJETIVOS",
+              1000,
+              "MISSÕES",
+              2000,
+            ]}
+            speed={10}
+            className="desktop:text-xs mobile:text-3xs text-center tablet:text-3xs laptop:text-3xs font-bold text-secondary-default"
+            repeat={Infinity}
+          />
+        </Link>
       </div>
     </div>
   );
