dce4cad0a556a3a6e985efceca03830014f2ebf7
Gustavo Veiga São Leandro <55115090+guta307@users.noreply.github.com>
2024-12-13 17:21:44 -0300

    feat: update Carroussel component with new projects and improved functionality
    - Replaced "Dinâmica da Face" project with a new "ERP" project, including updated title, description, image, and link.
    - Updated project links to point to live demos or LinkedIn posts for enhanced accessibility.
    - Extended slide transition interval from 7 seconds to 10 seconds for a smoother user experience.
    - Added `min-h-[280px]` to ensure a consistent minimum height for the carousel layout across devices.
    - Introduced `data-testid` attributes for navigation buttons to improve testability.
    - Refactored card initialization logic for improved readability and maintainability.
    - Included `array.length` as a dependency in the auto-slide effect for dynamic array handling.

--- src/stories/Organisms/Carroussel/Carroussel.tsx
+++ src/stories/Organisms/Carroussel/Carroussel.tsx
@@ -4,7 +4,7 @@ import { config } from "react-spring";
 import dynamic from "next/dynamic";
 import { Project } from "@/stories/Molecules/Project/Project";
 import JoaoPorfolio from "@/assets/images/Projects/Projeto _João.png"; // Ajuste o caminho conforme
-import DinamicaDaFace from "@/assets/images/Projects/Projeto_Doutora.png"; // Ajuste o caminho conforme
+import ERPImage from "@/assets/images/Projects/ERP.png"; // Ajuste o caminho conforme
 import FoodHub from "@/assets/images/Projects/FoodHub.png"; // Ajuste o caminho conforme
 import Alfabit from "@/assets/images/Projects/Alfabit.png"; // Ajuste o caminho conforme
 
@@ -27,7 +27,7 @@ export function Carroussel() {
           title="Web: Portfólio"
           description="Portfólio criado para um cinematógrafo exibir e compartilhar seus projetos."
           buttonText="acessar"
-          buttonLink="https://example.com"
+          buttonLink="https://joao-portfolio-sage.vercel.app/pages/home"
         />
       ),
     },
@@ -35,12 +35,12 @@ export function Carroussel() {
       key: 2,
       content: (
         <Project
-          imageSrc={DinamicaDaFace}
-          imageAlt="Image from Dinamica da Face website"
-          title="Web: Dinâmica da Face"
-          description="Site criado para um consultório odontológico e esteticista."
+          imageSrc={ERPImage}
+          imageAlt="Backend: ERP"
+          title="Backend: ERP"
+          description="Sistema de gestão empresarial com base em uma arquitetura de microserviços"
           buttonText="acessar"
-          buttonLink="https://example.com"
+          buttonLink="https://www.linkedin.com/feed/update/urn:li:activity:7264619310357094402/"
         />
       ),
     },
@@ -50,10 +50,10 @@ export function Carroussel() {
         <Project
           imageSrc={FoodHub}
           imageAlt="Image from Foodhub app"
-          title="Web: Portfólio"
+          title="App: Foodhub"
           description="Projeto mobile para um serviço de delivery."
           buttonText="acessar"
-          buttonLink="https://example.com"
+          buttonLink="https://www.linkedin.com/posts/gustavo-veiga-s%C3%A3o-leandro-2763651b1_reactnative-expo-storybook-activity-7175605037774102528-1kD6?utm_source=share&utm_medium=member_desktop"
         />
       ),
     },
@@ -62,11 +62,11 @@ export function Carroussel() {
       content: (
         <Project
           imageSrc={Alfabit}
-          imageAlt="Image from Foodhub app"
+          imageAlt="Image from Alfabit website"
           title="Web: Portfólio"
-          description="Projeto mobile para um serviço de delivery."
+          description="Projeto web utilizando storybook para documentação."
           buttonText="acessar"
-          buttonLink="https://example.com"
+          buttonLink="https://www.linkedin.com/posts/gustavo-veiga-s%C3%A3o-leandro-2763651b1_nextjs-tailwindcss-storybook-activity-7151324271225569280-7yhx?utm_source=share&utm_medium=member_desktop"
         />
       ),
     },
@@ -77,8 +77,14 @@ export function Carroussel() {
   });
   const [offsetRadius, setOffsetRadius] = useState(2);
   const [goToSlide, setGoToSlide] = useState<number | undefined>(undefined);
-  const [cards] = useState(table);
+  const [cards] = useState(
+    array.map((element, index) => ({
+      ...element,
+      onClick: () => setGoToSlide(index),
+    }))
+  );
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [timeoutId, setTimeoutId] = useState(null); // Store the timeout ID for clearing
 
   useEffect(() => {
     if (goToSlide !== undefined) {
@@ -91,13 +97,13 @@ export function Carroussel() {
       setGoToSlide((prevIndex) =>
         prevIndex === undefined ? 0 : (prevIndex + 1) % array.length
       );
-    }, 7000); // Change slide every 3 seconds
+    }, 10000); // Change slide every 10 seconds
 
     return () => clearInterval(interval);
-  }, []);
+  }, [array.length]); // Inclua 'array.length' como dependência
 
   return (
-    <div className="flex w-[90vw] mobile:h-[280px] tablet:h-[500px] desktop:h-[450px] flex-col justify-evenly">
+    <div className="flex w-[90vw] mobile:h-[280px] tablet:h-[500px] desktop:h-[450px] min-h-[280px] flex-col justify-evenly">
       <DynamicCarousel
         slides={cards}
         goToSlide={goToSlide}
@@ -109,6 +115,7 @@ export function Carroussel() {
       <div className="dots z-10 flex  ">
         {array.map((_, index) => (
           <button
+            data-testid={`navButton-${index}`}
             key={index}
             className={`dot ${index === currentIndex ? "dot--active" : ""}`}
             onClick={() => setGoToSlide(index)}
