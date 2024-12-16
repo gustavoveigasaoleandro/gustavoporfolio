7600a664ca811614609dc0e1b35e3b1532432dca
Gustavo Veiga São Leandro <55115090+guta307@users.noreply.github.com>
2024-12-16 09:03:19 -0300

    refactor: replace PopUp modal with Drawer component for improved behavior and control

--- src/stories/Organisms/PopUpTrigger/PopUpTrigger.tsx
+++ src/stories/Organisms/PopUpTrigger/PopUpTrigger.tsx
@@ -1,34 +1,49 @@
 "use client";
+import { useState } from "react";
+import {
+  Drawer,
+  DrawerContent,
+  DrawerTrigger,
+  DrawerClose,
+} from "@/components/ui/drawer";
 import PopUpIcon from "@/assets/images/PopupTrigger/PopupTrigger.svg";
 import Image from "next/image";
-import "./style.css";
-import { useState } from "react";
 import { PopUp } from "../PopUp/PopUp";
-export const PopUpTrigger = () => {
-  const [ModalActive, setModalActive] = useState(false);
 
-  const TriggerModal = () => {
-    setModalActive(!ModalActive);
+export const PopUpTrigger = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
   };
 
   return (
-    <>
-      {ModalActive && (
-        <div className="z-20 fixed top-0 left-0 right-0 bottom-0 m-auto pt-10 flex justify-center items-center">
-          <PopUp />
-        </div>
-      )}
-
-      <div
-        onClick={TriggerModal}
-        className="z-30 desktop:w-xl fixed bottom-6 right-1 flex justify-center align-middle items-center desktop:h-[137px] mobile:w-[56px] mobile:h-[56px] bg-secondary-default rounded-full float"
+    <div data-testid="PopUpTriggerWithDrawer">
+      <Drawer
+        open={isOpen}
+        onOpenChange={setIsOpen}
+        direction="bottom"
+        dismissible={false}
       >
-        <Image
-          className="desktop:w-[96px] mobile:w-[40px]"
-          src={PopUpIcon}
-          alt="PopUp Icon"
-        />
-      </div>
-    </>
+        {/* Trigger para abrir o Drawer */}
+        <DrawerTrigger asChild>
+          <div
+            onClick={handleToggle}
+            className="laptop:z-30 mobile:z-20 desktop:w-xl fixed bottom-6 right-1 flex justify-center align-middle items-center desktop:h-[137px] mobile:w-[56px] mobile:h-[56px] bg-secondary-default rounded-full float cursor-pointer"
+          >
+            <Image
+              className="desktop:w-[96px] mobile:w-[40px]"
+              src={PopUpIcon}
+              alt="PopUp Icon"
+            />
+          </div>
+        </DrawerTrigger>
+
+        {/* Conteúdo do Drawer (PopUp) */}
+        <DrawerContent className="flex justify-center h-[100%] items-center">
+          <PopUp closeFunction={() => setIsOpen(false)} />
+        </DrawerContent>
+      </Drawer>
+    </div>
   );
 };
