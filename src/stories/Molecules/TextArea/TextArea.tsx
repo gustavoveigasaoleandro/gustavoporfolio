f8917552dde71b414ee5ab80afd19140c4cb0a7e
Gustavo Veiga São Leandro <55115090+guta307@users.noreply.github.com>
2024-12-17 12:47:19 -0300

    fix: resolve type incompatibility in TextAreaComponent
    - Added `TextAreaProps` interface to explicitly define props for `TextAreaComponent`.
    - Forced `onChange` to use `React.ChangeEventHandler<HTMLTextAreaElement>` to address type mismatch errors.
    - Improved code readability and ensured compatibility with the Textarea component.

--- src/stories/Molecules/TextArea/TextArea.tsx
+++ src/stories/Molecules/TextArea/TextArea.tsx
@@ -2,20 +2,29 @@ import { Text } from "../../Atoms/Text/Text";
 import { Label } from "@/components/ui/label";
 import { InputProps } from "@/interfaces/Input";
 import { Textarea } from "@/components/ui/textarea";
+import { TextAreaProps } from "@/interfaces/TextArea";
 
-export const TextAreaComponent = ({ label, id, name }: InputProps) => {
+export const TextAreaComponent = ({
+  label,
+  id,
+  name,
+  onChange,
+  ...rest
+}: TextAreaProps) => {
   return (
-    <div className="grid w-full  items-center gap-2">
+    <div className="grid w-full items-center gap-2">
       <Label htmlFor={id}>
         <Text color="white" type="medium">
           {"{"} {label} {"}"}
         </Text>
       </Label>
       <Textarea
-        className=" text-3xs resize-none  bg-light-default h-[113px]  w-full mobile:h-[20vh] "
+        className="text-3xs resize-none bg-light-default h-[113px] w-full mobile:h-[20vh]"
         name={name}
         id={id}
         placeholder=""
+        onChange={onChange as React.ChangeEventHandler<HTMLTextAreaElement>} // Forçamos o tipo correto
+        {...rest}
       />
     </div>
   );
